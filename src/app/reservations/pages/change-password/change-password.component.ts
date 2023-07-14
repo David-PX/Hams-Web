238a8db08d18721290842b3e12e41328c1cb06f0
siriuzz <elianmatosd03@gmail.com>
2023-07-14 14:58:33 -0400

    vista de cambiar contraseña

--- src/app/reservations/pages/change-password/change-password.component.ts
+++ src/app/reservations/pages/change-password/change-password.component.ts
@@ -1,10 +1,46 @@
 import { Component } from '@angular/core';
+import { Validator, FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { ValidatorsService } from 'src/app/shared/services/validators.service';
 
 @Component({
   selector: 'app-change-password',
   templateUrl: './change-password.component.html',
-  styleUrls: ['./change-password.component.scss']
+  styleUrls: ['./change-password.component.scss'],
 })
 export class ChangePasswordComponent {
+constructor(
+  private fb: FormBuilder,
+  private _validatorsService: ValidatorsService
 
+){}
+
+  public changePasswordForm: FormGroup = this.fb.group({
+    actualPassword: ['', [Validators.required, Validators.email]],
+    newPassword: ['', Validators.required],
+    newPassword2: ['', Validators.required],
+  });
+
+  isValidField(field: string) {
+    return this._validatorsService.isValidField(this.changePasswordForm, field);
+  }
+
+  getFieldError(field: string): string | null {
+    if (!this.changePasswordForm.controls[field]) return null;
+
+    const errors = this.changePasswordForm.controls[field].errors || {};
+
+    for (const key of Object.keys(errors)) {
+      switch (key) {
+        case 'required':
+          return 'Este campo es requerido';
+
+        case 'minlength':
+          return `Minimo ${errors['minlength'].requiredLength} caracteres`;
+
+        case 'email':
+          return `Correo Electronico Invalido`;
+      }
+    }
+    return null;
+  }
 }
